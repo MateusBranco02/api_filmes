6db05adae7eaea7f1f55485c6de259701e70454e
Mateus Silva Branco <143047033+MateusBranco02@users.noreply.github.com>
2024-09-30 19:52:39 -0300

    criando função cadastrarFilme

--- controller/filmes.js
+++ controller/filmes.js
@@ -10,4 +10,12 @@ const categoriaFilmes = async (req, res) => {
     const resultado = await database.query(`SELECT * FROM db_filmes.public.filmes WHERE categoria = '${categoria}'`);
     res.status(200).send(resultado);
 }
-export { listaFilmes, categoriaFilmes };
+
+const cadastrarFilmes = async (req, res) => {
+    const novoFilme = req.body;
+    const resultado = await database.query(`INSERT INTO db_filmes.public.filmes(id, titulo, categoria) VALUES ('id, titulo, categoria', ${novoFilme})`);
+    res.status(201).send({ mensagem: 'Filme Cadastrado!', dados: resultado });
+
+}
+
+export { listaFilmes, categoriaFilmes, cadastrarFilmes };
--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { listaFilmes, categoriaFilmes } from '../controller/filmes.js';
+import { listaFilmes, categoriaFilmes, cadastrarFilmes } from '../controller/filmes.js';
 
 const router = express.Router();
 
 router.get('/filmes', listaFilmes);
 router.get('/filmes/:categoria', categoriaFilmes);
+
+router.post('/filmes/cadastrar', cadastrarFilmes);
 
 export default router;
